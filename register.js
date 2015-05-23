f49cfe7e07aec1a000054c0d5c91a06a6dbfa26f
Alvaro Balmaceda <oravla@gmail.com>
2015-05-23 19:04:05 -0300

    First commit

---
+++ register.js
@@ -0,0 +1,8 @@
+// NOT CURRENTLY USED, AS IT WOULD REQUIRE A DEPENDENCY ON CORE.
+RealTimeCore.registerPackage({
+  name: 'rt-core-theme',
+  autoEnable: true
+  settings: {
+    mode: true,
+  }
+});
